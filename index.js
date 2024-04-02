1121c13876317fbaf59e21378f9ad71ed68ca582
Pavel Poroshin <p.poroshin@hybrid.ai>
2024-04-02 10:26:11 +0300

    Proxy server to bypass CORS

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const https = require('https');
+
+const app = express();
+
+app.use((req, res) => {
+    const URL_SPLIT = req.url.split('/')
+    let options = {
+        hostname: URL_SPLIT[1],
+        path: '/' + URL_SPLIT.slice(2, URL_SPLIT.length).join('/'),
+    };
+    console.log(options)
+    let proxy = https.request(options, function (targetRes) {
+        console.log(targetRes.statusCode)
+        targetRes.headers['Access-Control-Allow-Origin'] = '*'
+        res.writeHead(targetRes.statusCode, {
+            ...targetRes.headers, 
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        });
+        targetRes.pipe(res, {
+            end: true
+        });
+    });
+    req.pipe(proxy, {
+        end: true
+    });
+});
+
+app.listen(3000);
+
+console.log("Proxy server is running on localhost:3000");
